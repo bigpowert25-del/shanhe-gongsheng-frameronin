e129708e59f0a1138d3db2252acb7f47d83ab828
mac <mac@macdeMacBook-Air.local>
2026-07-16 15:48:03 +0800

    publish fast mobile web client

---
+++ mobile/index.service.worker.js
@@ -0,0 +1,18 @@
+// Compatibility updater for visitors previously controlled by the Godot worker.
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: "window" }))
+      .then((clients) => Promise.all(clients.map((client) => client.navigate(client.url)))),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method === "GET") {
+    event.respondWith(fetch(event.request));
+  }
+});
