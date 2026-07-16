569c24ac9e0c14e79177df8b7c44de2b706f83bb
mac <mac@macdeMacBook-Air.local>
2026-07-16 14:24:08 +0800

    Optimize mobile Web payload

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784180683|2892437';
+const CACHE_VERSION = '1784182910|21310382';
 /** @type {string} */
 const CACHE_PREFIX = 'Shanhe Gongsheng-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
